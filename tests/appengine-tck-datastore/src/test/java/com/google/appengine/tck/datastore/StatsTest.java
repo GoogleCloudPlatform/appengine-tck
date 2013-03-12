1b1b52fe8823a746a2e5efb4371e9fa5dcc3b94c
Ales Justin <ales.justin@gmail.com>
2013-03-12 17:41:43 +0100

    Add dummy test.

--- tests/appengine-tck-datastore/src/test/java/com/google/appengine/tck/datastore/StatsTest.java
+++ tests/appengine-tck-datastore/src/test/java/com/google/appengine/tck/datastore/StatsTest.java
@@ -2,26 +2,31 @@
 
 package com.google.appengine.tck.datastore;
 
-import static org.junit.Assert.assertTrue;
-
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Query;
-
 import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * datastore statistics test.
  * http://code.google.com/appengine/docs/java/datastore/stats.html
- *  
+ *
  * @author hchen@google.com (Hannah Chen)
  */
 @RunWith(Arquillian.class)
 public class StatsTest extends DatastoreTestBase {
-  private static final String[] stats = {"Total__", "Kind__", "Kind_IsRootEntity__", 
-    "Kind_NotRootEntity__", "PropertyType__", "PropertyType_Kind__", "PropertyName_Kind__",
-    "PropertyType_PropertyName_Kind__"};
+    private static final String[] stats = {"Total__", "Kind__", "Kind_IsRootEntity__",
+            "Kind_NotRootEntity__", "PropertyType__", "PropertyType_Kind__", "PropertyName_Kind__",
+            "PropertyType_PropertyName_Kind__"};
+
+    @Test
+    public void testDummy() {
+        // TODO -- remove once some real test is in place
+    }
 
 //  @Test
 //  public void testStatics() {
@@ -37,12 +42,12 @@ public class StatsTest extends DatastoreTestBase {
 //    }
 //  }
 
-  private void checkCount(String statsKind) {
-    FetchOptions fo = FetchOptions.Builder.withDefaults();
-    Query query = new Query(statsKind);
-    assertTrue(datastoreService.prepare(query).countEntities(fo) > 0);
-    for (Entity readRec : datastoreService.prepare(query).asIterable()) {
-      assertTrue((Long) readRec.getProperty("count") > 0);
+    private void checkCount(String statsKind) {
+        FetchOptions fo = FetchOptions.Builder.withDefaults();
+        Query query = new Query(statsKind);
+        assertTrue(datastoreService.prepare(query).countEntities(fo) > 0);
+        for (Entity readRec : datastoreService.prepare(query).asIterable()) {
+            assertTrue((Long) readRec.getProperty("count") > 0);
+        }
     }
-  }
 }
