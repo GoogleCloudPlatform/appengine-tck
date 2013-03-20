1503a6474cff27e96aaa7e77f3f92356bbcfdac0
Ales Justin <ales.justin@gmail.com>
2013-03-20 01:07:05 +0100

    Cache service loader.

--- common/appengine-tck-base/src/main/java/com/google/appengine/tck/event/TestLifecycles.java
+++ common/appengine-tck-base/src/main/java/com/google/appengine/tck/event/TestLifecycles.java
@@ -1,6 +1,8 @@
 package com.google.appengine.tck.event;
 
+import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.WeakHashMap;
 
 import com.google.appengine.tck.base.TestBase;
 import com.google.appengine.tck.base.TestContext;
@@ -12,12 +14,24 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class TestLifecycles {
+    private static Map<ClassLoader, ServiceLoader<TestLifecycle>> cache = new WeakHashMap<ClassLoader, ServiceLoader<TestLifecycle>>();
+
     protected static Iterable<TestLifecycle> getServicesLifecycles() {
         return getServicesLifecycles(TestBase.class.getClassLoader());
     }
 
     public static Iterable<TestLifecycle> getServicesLifecycles(ClassLoader cl) {
-        return ServiceLoader.load(TestLifecycle.class, cl);
+        ServiceLoader<TestLifecycle> sl = cache.get(cl);
+        if (sl == null) {
+            sl = ServiceLoader.load(TestLifecycle.class, cl);
+            cache.put(cl, sl);
+        }
+        return sl;
+    }
+
+    public static void reload(ClassLoader cl) {
+        ServiceLoader<TestLifecycle> sl = cache.get(cl);
+        if (sl != null) sl.reload();
     }
 
     public static void before(TestLifecycleEvent event) {
