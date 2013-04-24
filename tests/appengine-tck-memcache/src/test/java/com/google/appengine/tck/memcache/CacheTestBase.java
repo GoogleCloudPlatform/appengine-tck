ef5e1266a2a959ba7d1dc4d9b76aa45e054584fb
Terry Okamoto <terryok@google.com>
2013-04-23 18:29:24 -0700

    MemcacheService 100% method coverage, and some refactoring of async helper methods to CacheTestBase.

--- tests/appengine-tck-memcache/src/test/java/com/google/appengine/tck/memcache/CacheTestBase.java
+++ tests/appengine-tck-memcache/src/test/java/com/google/appengine/tck/memcache/CacheTestBase.java
@@ -4,7 +4,10 @@ package com.google.appengine.tck.memcache;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -24,6 +27,9 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 public abstract class CacheTestBase extends TestBase {
     protected static final String STR_VALUE = "str_value";
     protected static final String KEY1 = "key1";
+    protected static final int overhead = 1024;   // space for key value
+    protected static final String str1mb = getBigString(1024 * 1024 - overhead);
+    protected static final String str1K = getBigString(1024);
 
     // data of various types for testing -- primitive types, String, Date, Set,
     // array, custom type
@@ -56,8 +62,8 @@ public abstract class CacheTestBase extends TestBase {
 
     protected static final int[] ARRAY1 = {1, -1, 0};
     protected static final Object[] ARRAY2 = {1, STR_VALUE, COMBO2};
-
-    protected static final Object[] TEST_DATA = {KEY1, 101, -1, 0, 123456789L, -987654321L, new Date(0), COMBO1, EMPTY_SET, SET1};
+    protected static final Object[] TEST_DATA = {KEY1, 101, -1, 0, 123456789L, -987654321L, new Date(0),
+        COMBO1, EMPTY_SET, SET1};
 
     @Deployment
     public static WebArchive getDeployment() {
@@ -65,5 +71,61 @@ public abstract class CacheTestBase extends TestBase {
         war.addClass(CacheTestBase.class);
         war.addClass(ComboType.class);
         return war;
+    }
+
+    protected static String getBigString(int len) {
+        char[] chars = new char[len];
+        for (int i = 0; i < len; i++) {
+            chars[i] = 'x';
+        }
+        return new String(chars);
+    }
+
+    protected Map<Object, Object> createSmallBatchData() {
+        String tsKey = createTimeStampKey("SmallBatchData");
+        Map<Object, Object> map = new HashMap<Object, Object>();
+        for (int i = 0; i < 3; i++) {
+            map.put(tsKey + "-" + i, str1K);
+        }
+        return map;
+    }
+
+    protected Map<Object, Long> createLongBatchData() {
+        String tsKey = createTimeStampKey("LongBatchData");
+        Map<Object, Long> map = new HashMap<Object, Long> ();
+        for (long num = 0; num < 3; num++) {
+            map.put(tsKey + "-" + num, num);
+        }
+        return map;
+    }
+
+    protected Map<Object, Long> copyMapIncrementLongValue(Map<Object, Long> map, long delta) {
+        Map<Object, Long> copiedMap = new HashMap<Object, Long>();
+        for (Map.Entry<Object, Long> entry : map.entrySet()) {
+            copiedMap.put(entry.getKey(), entry.getValue() + delta);
+        }
+        return copiedMap;
+    }
+
+    protected Map<Object, Long> createRandomIncrementMap(Map<Object, Long> map) {
+        Map<Object, Long> incMap = new HashMap<Object, Long>();
+        Random random = new Random();
+
+        int max = 1000;
+        for (Map.Entry<Object, Long> entry : map.entrySet()) {
+            incMap.put(entry.getKey(), new Long(random.nextInt(max)));
+        }
+        return incMap;
+    }
+
+    protected Map<Object, Long> createMapFromIncrementMap(Map<Object, Long> originalMap,
+                                                        Map<Object, Long> incMap) {
+        Map<Object, Long> incrementedMap = new HashMap<Object, Long>();
+
+        for (Map.Entry<Object, Long> entry : originalMap.entrySet()) {
+            incrementedMap.put(entry.getKey(), entry.getValue() + incMap.get(entry.getKey()));
+        }
+
+        return incrementedMap;
     }
 }
