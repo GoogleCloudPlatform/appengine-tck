599cd9075eda4edf98fd6041f5516f3dc707eb15
Terry Okamoto <terryok@google.com>
2013-04-29 15:50:17 -0700

    UserService 100% method coverage.

--- tests/appengine-tck-users/src/test/java/com/google/appengine/tck/users/UserServiceTest.java
+++ tests/appengine-tck-users/src/test/java/com/google/appengine/tck/users/UserServiceTest.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.appengine.tck.users;
 
-import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.api.users.UserServiceFailureException;
@@ -8,15 +22,12 @@ import com.google.appengine.tck.users.support.ServletAnswer;
 import com.google.appengine.tck.util.AuthClientException;
 import com.google.appengine.tck.util.GaeAuthClient;
 import com.google.appengine.tck.util.Utils;
+import com.google.apphosting.api.ApiProxy;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.After;
 import org.junit.Assert;
@@ -25,21 +36,25 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
- * Users Service tests.
+ * There are 3 authentication settings.
+ *   1) Google Accounts
+ *   2) Google Apps Accounts
+ *   3) Federated Login/OpenId Provider
  *
- * dev_appserver does not actually authenticate a user, while appserver does.
- * <p/>
- * @author smithd@google.com
- * @author ales.justin@jboss.org
+ * This test class covers #1.  See https://developers.google.com/appengine/articles/auth
+ *
+ * Dev_appserver and appspot differences, dev_appserver does not actually
+ * authenticate a user, while appspot does.
+ *
  */
 @RunWith(Arquillian.class)
 public class UserServiceTest extends UserTestBase {
@@ -47,9 +62,43 @@ public class UserServiceTest extends UserTestBase {
     private UserService userService;
     private GaeAuthClient authClient;
 
+    private static final String DEST_URL = "http://gaetcktest.org?abc=123&xyz=456";
+
+    // dev_appserver also encodes : and /
+    private static final String DEST_URL_ENCODED_SINGLE = "http%3A%2F%2Fgaetcktest.org%3Fabc%3D123%26xyz%3D456";
+
+    private static final String DEST_URL_ENCODED_DOUBLE = "http://gaetcktest.org/%253Fabc%253D123%2526xyz%253D456";
+    private static final String DEST_URL_ENCODED_TRIPLE = "http://gaetcktest.org/%25253Fabc%25253D123%252526xyz%25253D456";
+    private String userId;
+    private String pw;
+    private Boolean prodEnv;
+    private Boolean devEnv;
+
     @Before
     public void setUp() {
         userService = UserServiceFactory.getUserService();
+        userId = System.getProperty("appengine.userId");
+        pw = System.getProperty("appengine.password");
+    }
+
+    private void initServletEnvironment(URL url) throws IOException {
+        ServletAnswer answer = getUnAuthServletAnswer(url, "env");
+        prodEnv = answer.isEnvironmentProd();
+        devEnv = !prodEnv;
+    }
+
+    private boolean isServletProd(URL url) throws IOException {
+       if (prodEnv == null) {
+           initServletEnvironment(url);
+       }
+       return prodEnv;
+    }
+
+    private boolean isServletDev(URL url) throws IOException {
+        if (devEnv == null) {
+            initServletEnvironment(url);
+        }
+        return devEnv;
     }
 
     @After
@@ -71,6 +120,7 @@ public class UserServiceTest extends UserTestBase {
 
     private ServletAnswer getAuthServletAnswer(GaeAuthClient client, URL url,
                                                String method) throws IOException {
+
         HttpResponse response = client.getUrl(url + "/user-service-helper?method=" + method);
         String resp = EntityUtils.toString(response.getEntity());
         return new ServletAnswer(resp);
@@ -88,72 +138,225 @@ public class UserServiceTest extends UserTestBase {
 
     @Test
     @RunAsClient
-    public void pingService(@ArquillianResource URL url) throws Exception {
-        ServletAnswer unAuthAnswer = getUnAuthServletAnswer(url, "env");
-
-        String userId = System.getProperty("appengine.userId");
-        String pw = System.getProperty("appengine.password");
-
-        if (unAuthAnswer.env.equals("Production")) {
-            initAuthClient(url, userId, pw);
-            ServletAnswer answer = getAuthServletAnswer(authClient, url, "getEmail");
-            Assert.assertEquals("UserId should be same as authenticated user:" + answer,
-                userId, answer.returnVal);
+    public void testGetEmailProd(@ArquillianResource URL url) throws Exception {
+        if (!isServletProd(url)) {
+            return;
         }
-    }
 
-
-    @Test
-    public void userServiceIsAvailable() {
-        UserService userService = UserServiceFactory.getUserService();
-        assertNotNull("Expecting user service to be available.", userService);
-    }
-
-    @Test
-    public void testSimple() throws Exception {
-        String destinationURL = UserServiceFactory.getUserService().createLoginURL("destinationURL");
-        assertNotNull(destinationURL);
+        initAuthClient(url, userId, pw);
+        ServletAnswer answer = getAuthServletAnswer(authClient, url, "getEmail");
+        Assert.assertEquals("UserId should be same as authenticated user:" + answer,
+            userId, answer.getReturnVal());
     }
 
     @Test
-    // TODO: Re-enable check of authenticated user.
-    public void testGetCurrentUser() {
-        User user = userService.getCurrentUser();
-        if (isRuntimeProduction()) {
-            //assertNotNull("User should be valid under prod.", user);
-            assertNull("No User should be valid under prod.", user);
-        } else {
-            assertNull("User should be null under dev_appserver", user);
+    @RunAsClient
+    public void testGetEmailDev(@ArquillianResource URL url) throws Exception {
+        if (!isServletDev(url)) {
+            return;
         }
+
+        ServletAnswer answer = getUnAuthServletAnswer(url, "getEmail");
+        Assert.assertEquals("user is null", answer.getReturnVal());
+    }
+
+    /**
+     * Verify isUserLoggedIn on production instance.
+     */
+    @Test
+    @RunAsClient
+    public void testIsUserLoggedInProd(@ArquillianResource URL url) throws Exception {
+        if (!isServletProd(url)) {
+            return;
+        }
+
+        initAuthClient(url, userId, pw);
+        ServletAnswer answer = getAuthServletAnswer(authClient, url, "isUserLoggedIn");
+        Assert.assertEquals("User should be Logged in since the GaeAuthClient is initialized.",
+            "true", answer.getReturnVal());
+    }
+
+    /**
+     * Verify isUserLoggedIn on dev_appserver.
+     */
+    @Test
+    @RunAsClient
+    public void testIsUserLoggedInDev(@ArquillianResource URL url) throws Exception {
+        if (!isServletDev(url)) {
+            return;
+        }
+
+        ServletAnswer answer = getUnAuthServletAnswer(url, "isUserLoggedIn");
+        Assert.assertEquals("false", answer.getReturnVal());
     }
 
     @Test
-    // TODO: Re-enable check of authenticated user.
-    public void testIsUserLoggedIn() {
+    @RunAsClient
+    public void testIsUserAdminInProd(@ArquillianResource URL url) throws Exception {
+        if (!isServletProd(url)) {
+            return;
+        }
+
+        initAuthClient(url, userId, pw);
+        ServletAnswer answer = getAuthServletAnswer(authClient, url, "isUserAdmin");
+        Assert.assertEquals("User should be logged in and admin.",
+            "true", answer.getReturnVal());
+    }
+
+    @Test
+    @RunAsClient
+    public void testIsUserAdminInDev(@ArquillianResource URL url) throws Exception {
+        if (!isServletDev(url)) {
+            return;
+        }
+
+        ServletAnswer answer = getUnAuthServletAnswer(url, "isUserLoggedIn");
+        Assert.assertEquals("false", answer.getReturnVal());
+    }
+
+    @Test
+    public void testCreateLoginUrlProd() throws Exception {
+        if (!isRuntimeProduction()) {
+            return;
+        }
+
+        String start = "https://www.google.com/accounts/ServiceLogin?service=ah";
+        String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL);
+
+        String failMsgStartsWith = "Prod url should start with: " + start + " but was: " + createdURL;
+        Assert.assertTrue(failMsgStartsWith, createdURL.startsWith(start));
+
+        String failMsgContains = "Prod url should contain: " + DEST_URL_ENCODED_DOUBLE + " but was: " + createdURL;
+        Assert.assertTrue(failMsgContains, createdURL.contains(DEST_URL_ENCODED_DOUBLE));
+    }
+
+    @Test
+    public void testCreateLoginUrlDev() throws Exception {
+        if (!isRuntimeDev()) {
+            return;
+        }
+
+        String destURLenc = "/_ah/login?continue=" + DEST_URL_ENCODED_SINGLE;
+        String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL);
+        Assert.assertEquals(destURLenc, createdURL);
+    }
+
+    @Test
+    public void testCreateLoginUrlDomainProd() throws Exception {
+        if (!isRuntimeProduction()) {
+            return;
+        }
+
+        String start = "https://www.google.com/accounts/ServiceLogin?service=ah";
+        String authDomain = "othergaetcktest.org";
+        String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL, authDomain);
+
+        // TODO: verify how to check for the authDomain.
+
+        String failMsgStartsWith = "Prod url should start with: " + start + " but was: " + createdURL;
+        Assert.assertTrue(failMsgStartsWith, createdURL.startsWith(start));
+
+        String failMsgContains = "Prod url should contain: " + DEST_URL_ENCODED_DOUBLE + " but was: " + createdURL;
+        Assert.assertTrue(failMsgContains, createdURL.contains(DEST_URL_ENCODED_DOUBLE));
+    }
+
+
+    @Test
+    public void testCreateLoginUrlDomainDev() throws Exception {
+        if (!isRuntimeDev()) {
+            return;
+        }
+
+        // TODO: verify how to check for the authDomain.
+        String authDomain = "othergaetcktest.org";
+        String destURLenc = "/_ah/login?continue=" + DEST_URL_ENCODED_SINGLE;
+        String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL, authDomain);
+        Assert.assertEquals(destURLenc, createdURL);
+    }
+
+    @Test
+    public void testCreateLoginUrlFederatedNotSetProd() throws Exception {
+        if (!isRuntimeProduction()) {
+            return;
+        }
+
+        // Assuming Authentication Type set to Google Accounts, so org should be blank.
+        String userOrg = ApiProxy.getCurrentEnvironment()
+            .getAttributes()
+            .get("com.google.appengine.api.users.UserService.user_organization").toString();
+        Assert.assertEquals("", userOrg);
+
+        String authDomain = "othergaetcktest.org";
+        String federatedIdentity = "FedIdentTest";
+        Set<String> attrRequest = new HashSet<String>();
+
+        // throws IllegalArgumentException since not set to Federated Identity.
+        Exception thrownException = null;
+        try {
+            String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL, authDomain, federatedIdentity, attrRequest);
+        } catch (Exception e) {
+            thrownException = e;
+        }
+        // Testing exception like this since we cannot use the junit annotation in this case.
+        Assert.assertEquals(IllegalArgumentException.class, thrownException.getClass());
+
+    }
+
+    @Test
+    public void testCreateLoginUrlFederatedNotSetDev() throws Exception {
+        if (!isRuntimeDev()) {
+            return;
+        }
+
+        // TODO: verify how to check for the authDomain.
+        String authDomain = "othergaetcktest.org";
+        String destURLenc = "/_ah/login?continue=" + DEST_URL_ENCODED_SINGLE;
+        String createdURL = UserServiceFactory.getUserService().createLoginURL(DEST_URL, authDomain);
+        Assert.assertEquals(destURLenc, createdURL);
+    }
+
+    @Test
+    public void testCreateLogoutUrlProd() throws Exception {
+        if (!isRuntimeProduction()) {
+            return;
+        }
+
+        String contains1 = "https://www.google.com/accounts/Logout";
+        String createdURL = UserServiceFactory.getUserService().createLogoutURL(DEST_URL);
+
+        String failMsgContains1 = "Prod url should contain: " + contains1 + " but was: " + createdURL;
+        Assert.assertTrue(failMsgContains1, createdURL.contains(contains1));
+
+        String failMsgContains2 = "Prod url should contain: " + DEST_URL_ENCODED_TRIPLE + " but was: " + createdURL;
+        Assert.assertTrue(failMsgContains2, createdURL.contains(DEST_URL_ENCODED_TRIPLE));
+    }
+
+    @Test
+    public void testCreateLogoutUrlDev() throws Exception {
+        if (!isRuntimeDev()) {
+            return;
+        }
+
+        String destURLenc = "/_ah/logout?continue=" + DEST_URL_ENCODED_SINGLE;
+        String createdURL = UserServiceFactory.getUserService().createLogoutURL(DEST_URL);
+        Assert.assertEquals(destURLenc, createdURL);
+    }
+
+    /**
+     * On both dev_appserver and appspot, no user should be logged in.  The Arquillian container
+     * does not execute the test under an authenticated user.
+     */
+     @Test
+    public void testUserShouldNotBeLoggedIn() {
         boolean loggedIn = userService.isUserLoggedIn();
-        if (isRuntimeProduction()) {
-            //assertTrue("User should be logged in under prod.", loggedIn);
-            assertFalse("No User should be logged in under prod.", loggedIn);
-        } else {
-            assertFalse("User should not be logged in under dev_appserver", loggedIn);
-        }
+        assertFalse("User should not be logged in for both dev_appserver and appspot.", loggedIn);
     }
 
-//  @Test
-//  // TODO: Re-enable check of admin authenticated user.
-//  public void testIsUserAdmin() {
-//    if (!isRuntimeProduction()) {
-//      thrown.expect(IllegalStateException.class);
-//    }
-//    boolean userAdmin = userService.isUserAdmin();
-//    if (isRuntimeProduction()) {
-//      assertTrue("Whoever is running this test in prod is an admin.", userAdmin);
-//    }
-//  }
-
-    @Test(expected = IllegalStateException.class) // per javadoc
-    // TODO: Remove after test runs with authenticated admin user.
-    public void testIsUserAdmin() {
+    /**
+     * User is not logged in so calling isUserAdmin() should throw an exception.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testUserShouldNotBeAdmin() {
         boolean userAdmin = userService.isUserAdmin();
     }
 
--- tests/appengine-tck-users/src/test/java/com/google/appengine/tck/users/support/UserServiceServlet.java
+++ tests/appengine-tck-users/src/test/java/com/google/appengine/tck/users/support/UserServiceServlet.java
@@ -1,10 +1,23 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.appengine.tck.users.support;
-
 
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.api.users.UserServiceFailureException;
 import com.google.appengine.api.utils.SystemProperty;
 
 import java.io.IOException;
@@ -14,39 +27,54 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * TODO: http://go/java-style#javadoc
+ * Used by tests marked with @RunAsClient, since it can log into the servlet authenticated and
+ * exercise the UserService.
  */
 public class UserServiceServlet extends HttpServlet {
 
+    private UserService userService;
+    private User user;
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        UserService userService = UserServiceFactory.getUserService();
+        userService = UserServiceFactory.getUserService();
+        user = userService.getCurrentUser();
         String env = SystemProperty.environment.value().toString();
         String responseMsg = env + ",";
 
         String method = req.getParameter("method");
 
-        if (method == null || method.equals("env")) {
+        if (method == null) {
+            resp.getWriter().print(responseMsg + "Error: Must set method parameter.");
+            return;
+        }
+
+        if (method.equals("env")) {
             responseMsg += env;
             resp.getWriter().print(responseMsg);
             return;
         }
 
-        User user = userService.getCurrentUser();
+        responseMsg += callMethod(method);
+        resp.getWriter().print(responseMsg);
+    }
+
+    private String callMethod(String method) {
         try {
             if (method.equals("getEmail")) {
-                responseMsg += user.getEmail();
+                if (user == null) {
+                    return "user is null";
+                } else {
+                    return user.getEmail();
+                }
             } else if (method.equals("isUserLoggedIn")) {
-                responseMsg += userService.isUserLoggedIn();
+                return "" + userService.isUserLoggedIn();
             } else if (method.equals("isUserAdmin")) {
-                responseMsg += userService.isUserAdmin();
-            } else if (method.equals("UserServiceFailureException")) {
-                throw new UserServiceFailureException("Test!");
+                return "" + userService.isUserAdmin();
             } else {
-                responseMsg += "UNRECOGNIZED METHOD:" + method;
+                return "UNRECOGNIZED METHOD:" + method;
             }
         } catch (Exception e) {
-            responseMsg += e.getMessage();
+            return method + ":" + e.toString();
         }
-        resp.getWriter().print(responseMsg);
     }
 }
