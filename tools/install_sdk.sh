cc58bd236abbf93c250432275dac492b771be7e3
Terry Okamoto <terryok@google.com>
2013-09-13 13:30:36 -0700

    Additional artifacts needed for custom SDK install.

--- tools/install_sdk.sh
+++ tools/install_sdk.sh
@@ -11,9 +11,11 @@ if [ "$#" -lt 1 ]; then
 fi
 
 # ARTIFACT_IDS pair with the JAR pattern
-ARTIFACT_IDS=(appengine-api-1.0-sdk appengine-jsr107cache appengine-tools-sdk)
-JAR_PATTERN=(appengine-api-1.0-sdk-*.jar appengine-jsr107cache-*.jar appengine-tools-api*.jar)
+
+ARTIFACT_IDS=(appengine-api-1.0-sdk appengine-tools-sdk appengine-endpoints appengine-testing appengine-api-stubs)
+JAR_PATTERN=(appengine-api-1.0-sdk-*.jar appengine-tools-api*.jar appengine-endpoints.jar appengine-testing.jar appengine-api-stubs.jar)
 GROUP_ID=com.google.appengine
+ARTIFACT_COUNT=${#ARTIFACT_IDS[@]}
 
 function listRepository() {
   pushd ~ > /dev/null
@@ -55,7 +57,8 @@ if [ -z $VERSION ]; then
     exit 1
 fi
 
-for i in $(seq 0 2); do
+ARTIFACT_COUNT_LIMIT=$(( $ARTIFACT_COUNT - 1 ))
+for i in $(seq 0 $ARTIFACT_COUNT_LIMIT); do
   artifactId=${ARTIFACT_IDS[$i]}
   jarLocation=$(find $SDK_ROOT/lib -name "${JAR_PATTERN[$i]}")
   if [ "$jarLocation" == "" ]; then
