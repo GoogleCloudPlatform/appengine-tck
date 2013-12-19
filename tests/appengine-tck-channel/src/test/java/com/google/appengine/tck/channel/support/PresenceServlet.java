64eb067cf7b42be976ccf3c6573b7dd556a81314
Terry Okamoto <terryok@google.com>
2013-12-19 12:43:30 -0800

    Complete Channel coverage: added timeout test, removed ChannelFailureException from coverage, added presense connect/disconnet handler.

---
+++ tests/appengine-tck-channel/src/test/java/com/google/appengine/tck/channel/support/PresenceServlet.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.tck.channel.support;
+
+import com.google.appengine.api.channel.ChannelPresence;
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ */
+public class PresenceServlet extends HttpServlet {
+
+    Logger log = Logger.getLogger(PresenceServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ChannelService channelService = ChannelServiceFactory.getChannelService();
+        ChannelPresence presence = channelService.parsePresence(req);
+        log.info(presence.clientId() + ":" + presence.isConnected());
+    }
+}
