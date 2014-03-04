fb787406952c92572f931b16644e9aa41337f892
Marko Luksa <marko.luksa@gmail.com>
2014-03-04 14:48:07 +0100

    Test if system properties and env variables are set if defined in appengine-web.xml

---
+++ core/misc/src/test/java/com/google/appengine/tck/misc/env/EnvVariablesTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.tck.misc.env;
+
+import com.google.appengine.tck.base.TestBase;
+import com.google.appengine.tck.base.TestContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author <a href="mailto:marko.luksa@gmail.com">Marko Luksa</a>
+ */
+@RunWith(Arquillian.class)
+public class EnvVariablesTest extends TestBase {
+
+    @Deployment
+    public static WebArchive getDeployment() {
+        return getTckDeployment(new TestContext()
+            .setAppEngineWebXmlFile("appengine-web-env-variables.xml"));
+    }
+
+    @Test
+    public void testEnvironmentVariables() {
+        assertEquals("customEnvVariableValue", System.getenv("customEnvVariable"));
+    }
+}
