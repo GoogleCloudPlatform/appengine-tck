caf2b25653c7f07c62809648db478066211465eb
Ales Justin <ales.justin@gmail.com>
2014-05-28 15:42:41 +0200

    Add simple benchmark tests.

---
+++ core/benchmark/src/test/java/com/google/appengine/tck/benchmark/ObjectifyBenchmarkTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.tck.benchmark;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import com.google.appengine.tck.base.TestBase;
+import com.google.appengine.tck.base.TestContext;
+import com.google.appengine.tck.benchmark.support.Data;
+import com.google.appengine.tck.lib.LibUtils;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Terry Okamoto
+ */
+@RunWith(Arquillian.class)
+public class ObjectifyBenchmarkTest extends TestBase {
+    private static final Random RANDOM = new Random();
+
+    private Objectify objectify;
+
+    @Deployment
+    public static WebArchive getDeployment() {
+        TestContext context = new TestContext().setUseSystemProperties(true).setCompatibilityProperties(TCK_PROPERTIES);
+        WebArchive war = getTckDeployment(context);
+        war.addClass(Data.class);
+        LibUtils libUtils = new LibUtils();
+        libUtils.addLibrary(war, "com.googlecode.objectify", "objectify");
+        libUtils.addLibrary(war, "com.google.guava", "guava");
+        return war;
+    }
+
+    @Before
+    public void setUp() {
+        ObjectifyService.register(Data.class);
+        objectify = ObjectifyService.ofy();
+    }
+
+    @After
+    public void tearDown() {
+        ObjectifyService.reset();
+    }
+
+    @Test
+    public void testInserts() throws Exception {
+        int N = Integer.parseInt(getTestSystemProperty("benchmark.datastore.size", "6000"));
+        log.info(String.format(">>>> N = %s", N));
+
+        List<Data> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            Data data = new Data();
+            data.setCount(i);
+            data.setDate(new Date());
+            data.setName(UUID.randomUUID().toString());
+
+            data.setStart(RANDOM.nextFloat());
+            data.setStop(RANDOM.nextFloat());
+
+            data.setDown(RANDOM.nextFloat());
+            data.setUp(RANDOM.nextFloat());
+
+            list.add(data);
+        }
+
+        long start = System.currentTimeMillis();
+        Map<Key<Data>, Data> saved = objectify.save().entities(list).now();
+        long end = System.currentTimeMillis();
+        long totalSeconds = (end - start) / 1000;
+
+        log.info(String.format(">>>> Save [%s] time: %ssec", N, totalSeconds));
+
+        Assert.assertEquals(list.size(), saved.size());
+    }
+}
---
+++ core/benchmark/src/test/java/com/google/appengine/tck/benchmark/support/Data.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.tck.benchmark.support;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@Entity
+public class Data implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+    @Index
+    private String name;
+    @Index
+    private Date date;
+    private float start;
+    private float stop;
+    private float up;
+    private float down;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getStart() {
+        return start;
+    }
+
+    public void setStart(float start) {
+        this.start = start;
+    }
+
+    public float getStop() {
+        return stop;
+    }
+
+    public void setStop(float stop) {
+        this.stop = stop;
+    }
+
+    public float getUp() {
+        return up;
+    }
+
+    public void setUp(float up) {
+        this.up = up;
+    }
+
+    public float getDown() {
+        return down;
+    }
+
+    public void setDown(float down) {
+        this.down = down;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
